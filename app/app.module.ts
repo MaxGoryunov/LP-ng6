4fe35e60ae7d2a63374798d503ce4e6188b8b6de
MaxGoryunov <40582951+MaxGoryunov@users.noreply.github.com>
2018-09-23 01:04:48 +0300

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GeneralComponent } from './general/general.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    GeneralComponent,
+    SkillsComponent,
+    FeedbackComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

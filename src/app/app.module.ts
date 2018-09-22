debe26858937151fd8d7e6a271317d3b373b2d7f
Max Goryunov <maximthehedgehog@yandex.ru>
2018-09-23 01:23:30 +0300

    files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { GeneralComponent } from './general/general.component';
+import { SkillsComponent } from './skills/skills.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GeneralComponent,
+    SkillsComponent,
+    FeedbackComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

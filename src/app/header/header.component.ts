b987a9bbb71067c217e2d7571d10d46c1b910389
Max Goryunov <maximthehedgehog@yandex.ru>
2018-10-05 00:53:15 +0300

    header links animation added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -35,4 +35,25 @@ export class HeaderComponent implements OnInit {
       });
     };
 
+    document.addEventListener("scroll", function ():void {
+      let main = document.getElementById("a_main");
+      if (document.documentElement.scrollTop < 540) {
+        main.classList.add("active");
+      } else {
+        main.classList.remove("active");
+      };
+      let skills = document.getElementById("a_skills");
+      if (document.documentElement.scrollTop > 540 && document.documentElement.scrollTop < 1190) {
+        skills.classList.add("active");
+      } else {
+        skills.classList.remove("active");
+      };
+      let callback = document.getElementById("a_callback");
+      if (document.documentElement.scrollTop > 1190) {
+        callback.classList.add("active");
+      } else {
+        callback.classList.remove("active");
+      };
+    })
+  };
 }

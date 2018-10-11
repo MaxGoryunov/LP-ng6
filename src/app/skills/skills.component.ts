4c27b94ccd6e07b79dc2e334ceb5847a7caa99f1
Max Goryunov <maximthehedgehog@yandex.ru>
2018-10-12 00:51:43 +0300

    skills animations tried

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-skills',
@@ -17,6 +18,32 @@ export class SkillsComponent implements OnInit {
         document.getElementById("2_background").style.backgroundAttachment="scroll";
       }
     })
+    document.addEventListener("scroll", ():void => {
+      let scrollTop = document.documentElement.scrollTop;
+      let s_figures:any = [].slice.call(document.getElementsByClassName("s_figure"));
+      for (let s_figure of s_figures) {
+        if (scrollTop >= 140 + s_figures.indexOf(s_figure)*132) {
+          s_figure.style.animation = "Appear-Up 1s";
+          s_figure.style.opacity = "1";
+        } else {
+          s_figure.style.animation = "none";
+          s_figure.style.opacity = "0";   
+        };
+        /*if (scrollTop < 140 + s_figures.indexOf(s_figure)*132) {
+          s_figure.style.opacity = "1";
+          s_figure.style.animation = "Disappear-Down 1s";
+        } else {
+          s_figure.style.opacity = "0";
+          s_figure.style.animation = "none";
+        };
+        if (scrollTop > 790 + s_figures.indexOf(s_figure)*132) {
+          s_figure.style.animation = "Disappear-Up 1s";
+        } else {
+          s_figure.style.animation = "none";
+        }*/
+      }
+      
+    });
   }
 
 }

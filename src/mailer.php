cc5c80c9c6f4761d9f9c3e0b8fe31414ae5e3cf0
Max Goryunov <maximthehedgehog@yandex.ru>
2018-10-29 01:45:12 +0300

    php form tested; form works

--- src/mailer.php
+++ src/mailer.php
@@ -1,23 +1,16 @@
-<?php 
-if(isset($_POST['submit'])) { 
-$to = "maximthehedgehog@yandex.com"; 
-$subject = "Контактная форма!"; 
- 
-$name_field = filter_var($_POST['name'], FILTER_SANITIZE_STRING); 
-$email_field = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
-$phone_field = filter_var($_POST['phone'], FILTER_SANITIZE_STRING); 
-$comment = filter_var($_POST['comment'], FILTER_SANITIZE_STRING);
- 
-$body = " From: $name_field\n\n E-Mail: $email_field\n\n Phone: $phone_field\n\n Message:\n\n $comment";
- 
-$send = mail($to, $subject, $body);
+<?php
 
-if ($send) {
-    echo "<br>";
-    echo "Большое спасибо! Скоро мы свяжемся с Вами!";
-} else {
-    echo "Ошибка!";
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "maximthehedgehog@yandex.ru";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$phone.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
 }
- 
-} 
-?>

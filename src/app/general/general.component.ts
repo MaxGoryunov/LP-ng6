0d2cbeb8aef18cb6f28897a32bbfe5114640ee51
Max Goryunov <maximthehedgehog@yandex.ru>
2018-10-29 22:57:03 +0300

    window resize check added

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -27,24 +27,54 @@ export class GeneralComponent implements OnInit {
     let text = document.getElementById("fadeText");
     let blockHeight = window.innerHeight;
     let topScroll = document.documentElement.scrollTop;
-    document.addEventListener("scroll", ():void => {
-      img.style.transform = "translateX(" + ( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
-      text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";
-      if (document.documentElement.scrollTop >= (window.innerHeight + 50)/2.2) {
-        text.style.opacity = "0";
-        text.style.transform =  "translateX(100px)";
+    let windowSize = ():void => {
+      if (window.innerWidth > 480) {
+        document.addEventListener("scroll", ():void => {
+          img.style.transform = "translateX(" + ( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
+          text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";
+          if (document.documentElement.scrollTop >= (window.innerHeight + 50)/2.2) {
+            text.style.opacity = "0";
+            text.style.transform =  "translateX(100px)";
+          } else {
+            text.style.opacity = "1";
+            text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";        
+          }
+          if (document.documentElement.scrollTop >= (window.innerHeight + 50)/1.7) {
+            img.style.opacity = "0";
+            img.style.transform = "translateX(-100px)";
+          } else {
+            img.style.opacity = "1";
+            img.style.transform = "translateX(" +( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
+          }
+        })
       } else {
-        text.style.opacity = "1";
-        text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";        
+        document.removeEventListener("scroll", ():void => {
+          img.style.transform = "translateX(" + ( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
+          text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";
+          if (document.documentElement.scrollTop >= (window.innerHeight + 50)/2.2) {
+            text.style.opacity = "0";
+            text.style.transform =  "translateX(100px)";
+          } else {
+            text.style.opacity = "1";
+            text.style.transform = "translateX(" + 70*document.documentElement.scrollTop/blockHeight + "px)";        
+          }
+          if (document.documentElement.scrollTop >= (window.innerHeight + 50)/1.7) {
+            img.style.opacity = "0";
+            img.style.transform = "translateX(-100px)";
+          } else {
+            img.style.opacity = "1";
+            img.style.transform = "translateX(" +( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
+          }
+        })
       }
-      if (document.documentElement.scrollTop >= (window.innerHeight + 50)/1.7) {
-        img.style.opacity = "0";
-        img.style.transform = "translateX(-100px)";
-      } else {
-        img.style.opacity = "1";
-        img.style.transform = "translateX(" +( - 70*document.documentElement.scrollTop/blockHeight) + "px)";
-      }
-    })
+    }
+    window.onload = ():void => {
+      windowSize();
+    }
+    window.onresize = ():void => {
+      windowSize();
+    }
+    
   }
 
 }
